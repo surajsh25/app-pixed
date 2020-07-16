e6dee26a9c5a98bccc100a029dcfdf61fe4b83a1
surajsh25 <surajsh0395@gmail.com>
2020-07-16 19:13:50 +0530

    Initial commit

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ global.fetch = require('node-fetch');
 const Unsplash = require('unsplash-js').default;
 const toJson = require('unsplash-js').toJson;
 const express = require('express');
+const path = require('path');
 
 require('dotenv').config();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 let apikey = process.env.UNSPLASH_API_KEY;
 
@@ -19,4 +22,10 @@ app.get('/api/photos', (req, res) => {
     .then(json => res.json(json));
 });
 
-app.listen(5000, () => console.log('Started server at 5000'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Started server at 5000'));
